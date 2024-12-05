a57397f68f12950d61be4f9240edbfc48450ffd5
JoaquinDSP <146046416+JoaquinDSP@users.noreply.github.com>
2024-12-05 16:24:35 -0300

    Completed menu items

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -3,60 +3,134 @@ import { Component } from '@angular/core';
 import { LayoutService } from './service/app.layout.service';
 
 @Component({
-    selector: 'app-menu',
-    templateUrl: './app.menu.component.html'
+  selector: 'app-menu',
+  templateUrl: './app.menu.component.html',
 })
 export class AppMenuComponent implements OnInit {
+  model: any[] = [];
 
-    model: any[] = [];
+  constructor(public layoutService: LayoutService) {}
 
-    constructor(public layoutService: LayoutService) { }
-
-    ngOnInit() {
-        this.model = [
-            {
-                label: '    ', //title over "inicio"
-                items: [
-                    { label: 'Inicio', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
-                ]
-            },
-            {
-                label: 'Equipos',
-                items: [
-                    { label: 'Categorias', icon: 'pi pi-fw pi-users', routerLink: ['/uikit/formlayout'] },
-                ]
-            },
-            {
-                label: 'Prime Blocks',
-                items: [
-                    { label: 'Free Blocks', icon: 'pi pi-fw pi-eye', routerLink: ['/blocks'], badge: 'NEW' },
-                    { label: 'All Blocks', icon: 'pi pi-fw pi-globe', url: ['https://www.primefaces.org/primeblocks-ng'], target: '_blank' },
-                ]
-            },
-            {
-                label: 'Utilities',
-                items: [
-                    { label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', routerLink: ['/utilities/icons'] },
-                    { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: ['https://www.primefaces.org/primeflex/'], target: '_blank' },
-                ]
-            },
-            {
-                label: 'Pages',
-                icon: 'pi pi-fw pi-briefcase',
-                items: [
-                    {
-                        label: 'Landing',
-                        icon: 'pi pi-fw pi-globe',
-                        routerLink: ['/landing']
-                    },
-                    {
+  ngOnInit() {
+    this.model = [
+      {
+        label: '    ', //title over "inicio"
+        items: [
+          { label: 'Inicio', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
+        ],
+      },
+      {
+        label: 'Equipos',
+        items: [
+          {
+            label: 'Copa +50',
+            icon: 'fa-solid fa-trophy',
+            items: [
+              {
+                label: 'Diablitos Rojos',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Toros Rosso',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Charmander',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Vito Corleone FC',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Rata Blanca Team',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Craneos Despelechados',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+            ],
+          },
+          {
+            label: 'Copa Comercial',
+            icon: 'fa-solid fa-trophy',
+            items: [
+              {
+                label: 'Bondiola de Carne',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Ramonestones',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Calandria Team',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Chimangos locos',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Jaimicos fuchibol',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+              {
+                label: 'Retorcidos FC',
+                icon: 'fa-solid fa-shield',
+                routerLink: ['/uikit/formlayout'],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Fixture',
+        items: [
+          { label: 'Copa +50', icon: 'fa-regular fa-calendar-days' },
+          { label: 'Copa Comercial', icon: 'fa-regular fa-calendar-days' },
+        ],
+      },
+      {
+        label: 'Documentos',
+        items: [
+          {
+            label: 'Reglameto',
+            icon: 'fa-solid fa-gavel',
+            routerLink: ['/uikit/formlayout'],
+          },
+          {
+            label: 'Boletin',
+            icon: 'fa-solid fa-newspaper',
+            url: ['https://www.sensacine.com/peliculas/pelicula-111112/'],
+            target: '_self',
+          },
+        ],
+      },
+      {
+        label: 'Contactos',
+        icon: 'pi pi-fw pi-briefcase',
+        items: [
+          /*{
                         label: 'Auth',
                         icon: 'pi pi-fw pi-user',
                         items: [
                             {
                                 label: 'Login',
                                 icon: 'pi pi-fw pi-sign-in',
-                                routerLink: ['/auth/login']
+                                routerLink: ['/auth/login']         //keep cause is desplegable, idk
                             },
                             {
                                 label: 'Error',
@@ -69,82 +143,32 @@ export class AppMenuComponent implements OnInit {
                                 routerLink: ['/auth/access']
                             }
                         ]
-                    },
-                    {
-                        label: 'Crud',
-                        icon: 'pi pi-fw pi-pencil',
-                        routerLink: ['/pages/crud']
-                    },
-                    {
-                        label: 'Timeline',
-                        icon: 'pi pi-fw pi-calendar',
-                        routerLink: ['/pages/timeline']
-                    },
-                    {
-                        label: 'Not Found',
-                        icon: 'pi pi-fw pi-exclamation-circle',
-                        routerLink: ['/notfound']
-                    },
-                    {
-                        label: 'Empty',
-                        icon: 'pi pi-fw pi-circle-off',
-                        routerLink: ['/pages/empty']
-                    },
-                ]
-            },
+                    },*/
             {
-                label: 'Hierarchy',
-                items: [
-                    {
-                        label: 'Submenu 1', icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {
-                                label: 'Submenu 1.1', icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' },
-                                ]
-                            },
-                            {
-                                label: 'Submenu 1.2', icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }
-                                ]
-                            },
-                        ]
+                        label: 'WhatsApp',
+                        icon: 'fa-brands fa-whatsapp',
+                        url: 'https://wa.me/+5493413139012?text=%C2%A1Hola!%20Me%20comunico%20con%20Liga%20de%20Campo%20a%20fines%20de%20acercarles%20una%20consulta.',
+                        target: '_blank'
                     },
-                    {
-                        label: 'Submenu 2', icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {
-                                label: 'Submenu 2.1', icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' },
-                                ]
-                            },
-                            {
-                                label: 'Submenu 2.2', icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' },
-                                ]
-                            },
-                        ]
-                    }
-                ]
-            },
-            {
-                label: 'Get Started',
-                items: [
-                    {
-                        label: 'Documentation', icon: 'pi pi-fw pi-question', routerLink: ['/documentation']
-                    },
-                    {
-                        label: 'View Source', icon: 'pi pi-fw pi-search', url: ['https://github.com/primefaces/sakai-ng'], target: '_blank'
-                    }
-                ]
-            }
-        ];
-    }
+                    
+          {
+            label: 'Email',
+                        icon: 'fa-regular fa-envelope',
+                        url: 'https://wa.me/+5493413139012?text=%C2%A1Hola!%20Me%20comunico%20con%20Liga%20de%20Campo%20a%20fines%20de%20acercarles%20una%20consulta.',
+                        target: '_blank'
+          },
+          {
+            label: 'Instagram',
+            icon: 'fa-brands fa-instagram',
+            routerLink: ['/notfound'],
+          },
+          {
+            label: 'Facebook',
+            icon: 'fa-brands fa-facebook',
+            routerLink: ['/notfound'],
+          },
+        ],
+      },
+    ];
+  }
 }
