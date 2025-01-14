b1486f7968cc35fd240bbfc2ecf37a8a808d38af
JoaquinDSP <146046416+JoaquinDSP@users.noreply.github.com>
2025-01-14 16:27:53 -0300

    minor changes on images

---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Carousel } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { Tag } from 'primeng/tag';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.scss'
+})
+export class CarouselComponent {
+
+}
